f475614063554c9bf03fc6760d03e4d6f048b674
Le Huong <le308934@gmail.com>
2023-10-04 14:21:14 +0700

    Add project files.

---
+++ Models/Major.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab4.Models
+{
+    public class Major
+    {
+        public int MajorID { get; set; }
+        public string MajorName { get; set; }
+        public ICollection<Learner> Learners { get; set; }
+    }
+}
